90ae64fbe56296d0d575687c133492b731614c90
Antonio Junior <junyor@hotmail.co.uk>
2016-02-17 16:58:47 -0200

    Setting encoding UTF-8.

--- config.rb
+++ config.rb
@@ -7,6 +7,9 @@ output_style = :compressed
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
+# Setting charset to CSS generated file, this is really important to server
+Encoding.default_external = "UTF-8"
+
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
 
